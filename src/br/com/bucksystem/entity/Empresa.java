ba518638ad8aaf50fede684c76830b31c6872119
Rogerio Costa <rogerio.costa@bucksystem.com.br>
2014-09-01 15:01:02 -0300

    alteracao feita..

--- src/br/com/bucksystem/entity/Empresa.java
+++ src/br/com/bucksystem/entity/Empresa.java
@@ -1,9 +1,21 @@
 package br.com.bucksystem.entity;
 
 import java.io.Serializable;
-import javax.persistence.*;
 import java.sql.Timestamp;
 import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Version;
 
 
 /**
